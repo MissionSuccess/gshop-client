eefda22ab57ae02f035cffab87b5d51176ec022e
gx <1261726088@qq.com>
2019-02-19 17:24:37 +0800

    clear user data

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FooterGuide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import FooterGuide from "./components/FooterGuide/FooterGuide.vue";
+
+export default {
+  created() {
+    // 异步获取 address
+    // this.$store.dispatch("getAddress");
+    // 异步获取登陆用户信息
+    // this.$store.dispatch("getUserInfo");
+  },
+  mounted() {
+    //   this.$store.dispatch('getAddress');
+    this.getAddress();
+    this.getUserInfo();
+  },
+
+  methods: {
+    ...mapActions(['getAddress','getUserInfo'])
+  },
+
+  components: {
+    FooterGuide
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+#app {
+  width: 100%;
+  height: 100%;
+  background: #f5f5f5;
+}
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,18 @@
+/*
+入口JS
+*/
+import Vue from 'vue'
+import { Button } from 'mint-ui'
+
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.component(Button.name, Button)
+
+new Vue({
+    el: '#app',
+    render: h => h(App),
+    router,
+    store,
+})
